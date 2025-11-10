98db0e517e89392990c151ba65a919d79b00405d
veryspry <ehlinger.matt@gmail.com>
2025-11-11 00:56:59 +0100

    binary search impl.

--- src/binary-search.zig
+++ src/binary-search.zig
@@ -1,1 +1,43 @@
+const std = @import("std");
 
+fn getMidPoint(list: []const i32) usize {
+	return if (list.len % 2 == 0) list.len / 2 else (list.len - 1) / 2;
+}
+
+fn binarySearch(list: []const i32, target: i32) i32 {
+	var left: usize = 0;
+	var right: usize = list.len - 1;
+
+	while (left <= right) {
+		const mid = left + (right - left) / 2;
+
+		if (list[mid] < target) {
+			left = mid + 1;
+		} else if (list[mid] > target) {
+			if (mid <= 0) break; // typeof usize cannot be negative
+			right = mid - 1;
+		} else {
+			return @intCast(mid);
+		}
+	}
+
+	return -1;
+}
+
+test "binarySearch()" {
+    const arr1 = [_]i32{1, 2, 3, 4, 5};
+    try std.testing.expectEqual(2, binarySearch(&arr1, 3));
+
+    const arr2 = [_]i32{1, 2, 3, 4, 5, 6};
+    try std.testing.expectEqual(4, binarySearch(&arr2, 5));
+
+    const arr3 = [_]i32{1, 2, 3, 4, 5};
+    try std.testing.expectEqual(-1, binarySearch(&arr3, 6));
+
+    const arr4 = [_]i32{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    try std.testing.expectEqual(0, binarySearch(&arr4, 'a'));
+    try std.testing.expectEqual(25, binarySearch(&arr4, 'z'));
+    try std.testing.expectEqual(-1, binarySearch(&arr4, 'A'));
+    try std.testing.expectEqual(-1, binarySearch(&arr4, '}'));
+    try std.testing.expectEqual(6, binarySearch(&arr4, 'g'));
+}
--- src/minimum-subarray.zig
+++ src/minimum-subarray.zig
@@ -42,29 +42,93 @@ fn minimumSubarray(list: []const i32, target: i32) usize {
 	return result;
 }
 
-test "minimumSubarray" {
+fn min(a: usize, b: usize) usize {
+	return if (a < b) a else b;
+}
+
+fn minimumSubarrayWindow(list: []const i32, target: i32) usize {
+	const infinity = std.math.maxInt(usize);
+	var result: usize = infinity;
+
+	var leftIdx: usize = 0;
+	var rightIdx: usize = 1;
+
+	// temp sum for use with windows
+	var sum: i32 = list[leftIdx] + list[rightIdx];
+
+	while (leftIdx < list.len) {
+		// if sum equals target we have a new result to check and pointers to move
+		// if sum is greater than target we need to shrink the window from the left
+		// if sum is less than target we need to increase the window from the right
+
+		if (list[leftIdx] == target or list[rightIdx] == target) {
+			result = 1;
+			break;
+		}
+
+		std.debug.print("Result: {}, Sum: {}\n", .{result, sum});
+
+		if (sum < target) {
+			if (rightIdx == list.len - 1) {
+				std.debug.print("No result, right at the end. leftIdx: {}, rightIdx: {}\n", .{leftIdx, rightIdx});
+				// if right is at end and sum is still less than target,
+				// we can't find a(nother) solution
+				break;
+			} else {
+				std.debug.print("Expanding window to the right. leftIdx: {}, rightIdx: {}\n", .{leftIdx, rightIdx});
+				// expand the window to the right and check new sum
+				rightIdx = rightIdx + 1;
+				sum = sum + list[rightIdx];
+				continue;
+			}
+		}
+
+		if (sum == target) {
+			result = min(result, rightIdx - leftIdx + 1);
+			std.debug.print("Found target. leftIdx: {}, rightIdx: {}, result: {}\n", .{leftIdx, rightIdx, result});
+		} else {
+			std.debug.print("Sum is greater than target. leftIdx: {}, rightIdx: {}\n", .{leftIdx, rightIdx});
+		}
+
+		// if a target was reached or exceeded, reset the window and keep trying
+		leftIdx = if (leftIdx + 1 < list.len - 1) leftIdx + 1 else leftIdx;
+		rightIdx = if (leftIdx + 1 < list.len - 1) leftIdx + 1 else leftIdx;
+
+		sum = list[leftIdx] + list[rightIdx];
+	}
+
+	return if (result == infinity) 0 else result;
+}
+
+
+test "minimumSubarray() and minimumSubarrayWindow()" {
 	// target: 7
 	// expect: 2
 	var arr1 = [6]i32{2,3,1,2,4,3};
-	try std.testing.expectEqual(2, minimumSubarray(&arr1, 7));
+	// try std.testing.expectEqual(2, minimumSubarray(&arr1, 7));
+	try std.testing.expectEqual(2, minimumSubarrayWindow(&arr1, 7));
 
 	// target: 4
 	// expect: 1
 	var arr2 = [3]i32{1,4,4};
-	try std.testing.expectEqual(1, minimumSubarray(&arr2, 4));
+	// try std.testing.expectEqual(1, minimumSubarray(&arr2, 4));
+	try std.testing.expectEqual(1, minimumSubarrayWindow(&arr2, 4));
 
 	// target: 11
 	// expect: 0
 	var arr3 = [8]i32{1,1,1,1,1,1,1,1};
-	try std.testing.expectEqual(0, minimumSubarray(&arr3, 11));
+	// try std.testing.expectEqual(0, minimumSubarray(&arr3, 11));
+	try std.testing.expectEqual(0, minimumSubarrayWindow(&arr3, 11));
 
 	// target: 5
 	// expect: 2
 	var arr4 = [5]i32{2, 3, 2, 2, 1};
-	try std.testing.expectEqual(2, minimumSubarray(&arr4, 5));
+	// try std.testing.expectEqual(2, minimumSubarray(&arr4, 5));
+	try std.testing.expectEqual(2, minimumSubarrayWindow(&arr4, 5));
 
 	// target: 5
 	// expect: 1
 	var arr5 = [5]i32{5, 1, 2, 2, 3};
-	try std.testing.expectEqual(1, minimumSubarray(&arr5, 5));
+	// try std.testing.expectEqual(1, minimumSubarray(&arr5, 5));
+	try std.testing.expectEqual(1, minimumSubarrayWindow(&arr5, 5));
 }
